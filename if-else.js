2e07740f0ca1901eb526245e21a2a405e070eefe
Yopa Nelly <121314923+YopaNelly@users.noreply.github.com>
2023-03-08 17:34:48 +0200

    Update and rename if-else in javascript to if-else.js

---
+++ if-else.js
@@ -0,0 +1,21 @@
+console.log('hello! world')
+function testSize(num) {
+    // Only change code below this line
+    if(num<5){
+      return "Tiny"
+    }
+    else if(num<10){
+      return "Small"
+    }
+    else if(num<15){
+      return "Medium"
+    }
+    else if(num<20){
+      return "Large"
+    }
+    else if(num>=20){
+      return "Huge"
+    }
+  
+  
+    return "Change Me";
